5e45bfbfd8da467eee1174d5faf297843bbefdc6
Maria.Dias <maria.dias@somagrupo.com.br>
2022-11-27 22:25:19 -0300

    test: app get test

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
-app.listen(3333, () => {
+const server = app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
+
+export default server;
---
+++ src/tests/app.spec.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import server from '../app';
+describe('Get Endpoints', () => {
+  it('Should be able to get', async () => {
+    const res = await request(server).get('/').send();
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('message');
+  });
+});
+afterAll((done) => {
+  // close server conection
+  server.close();
+  done();
+});
